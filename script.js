bdee2a7dbf56baddb1e699dfca8f9e512e27d142
João Guimarães <112285486+joaoguims@users.noreply.github.com>
2024-01-13 17:54:11 -0300

    switch mode

---
+++ script.js
@@ -0,0 +1,16 @@
+function toggleMode(){
+  const html= document.documentElement
+  html.classList.toggle("ligth")
+
+  // pegar a tag img
+ /* const img = document.querySelector("#profile img")
+
+  //subtituir a imagem
+  if (html.classList.contains("ligth")){
+    //se tiver ligth mode, adicionar a imagem ligth
+    img.setAttribute("scr", "./assets/avatar-ligth.png")
+  } else {
+    //se tiver sem ligth mode, manter imagem normal
+    img.setAttribute("scr", "./assets/avatar.jpg")
+  }*/
+}
